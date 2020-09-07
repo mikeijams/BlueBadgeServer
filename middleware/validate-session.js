a538143dbe4d99d04267ee136697dba8b8648b94
Michael Ijams <mikeijams@Michaels-MacBook-Pro.local>
2020-09-07 19:56:45 -0400

    message

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,37 +1,26 @@
-const jwt = require("jsonwebtoken");
-const User = require("../db").import("../models/user");
-
-const validateSession = (req, res, next) => {
-  const token = req.headers.authorization;
+var jwt = require("jsonwebtoken");
+var sequelize = require("../db");
+var User = sequelize.import("../models/user");
+module.exports = function (req, res, next) {
   if (req.method == "OPTIONS") {
     next();
   } else {
-    console.log("token ---->", token);
-    if (!token) {
-      return res
-        .status(403)
-        .send({ auth: false, message: "No token Provided" });
-    } else {
-      jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
-        console.log("decodetoken---->", decodeToken);
-        if (!err && decodeToken) {
-          User.findOne({
-            where: {
-              id: decodeToken.id,
-            },
-          })
-            .then((user) => {
-              console.log("user ---->", user);
-              if (!user) throw err;
-              console.log("req ----->", req);
-              req.user = user;
-              return next();
-            })
-            .catch((err) => next(err));
-        } else {
-          req.errors = err;
-          return res.status(500).send("Not Authorized");
-        }
+    var sessionToken = req.headers.authorization;
+    console.log(sessionToken);
+    sessionToken
+      ? verifyToken()
+      : res.status(403).send({ auth: false, message: "No Token Provided" });
+    function verifyToken() {
+      jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => {
+        decoded
+          ? findUser(decoded)
+          : res.status(401).send({ error: "Not Authorized" });
+      });
+    }
+    function findUser(decoded) {
+      User.findOne({ where: { id: decoded.id } }).then((user) => {
+        req.user = user;
+        next();
       });
     }
   }
