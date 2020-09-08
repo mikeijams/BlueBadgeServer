4ac2ae144bdf99cba2d1299aba386c99f875f0f9
Michael Ijams <mikeijams@Michaels-MacBook-Pro.local>
2020-09-07 20:11:00 -0400

    message

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,29 +1,34 @@
-var jwt = require("jsonwebtoken");
-var sequelize = require("../db");
-var User = sequelize.import("../models/user");
-module.exports = function (req, res, next) {
-  if (req.method == "OPTIONS") {
-    next();
+const jwt = require("jsonwebtoken");
+const User = require("../db").import("../models/user");
+const validateSession = (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log("token -->", token);
+  if (!token) {
+    return res.status(403).send({ auth: false, message: "No token provided" });
   } else {
-    var sessionToken = req.headers.authorization;
-    console.log(sessionToken);
-    sessionToken
-      ? verifyToken()
-      : res.status(403).send({ auth: false, message: "No Token Provided" });
-    function verifyToken() {
-      jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => {
-        decoded
-          ? findUser(decoded)
-          : res.status(401).send({ error: "Not Authorized" });
-      });
-    }
-    function findUser(decoded) {
-      User.findOne({ where: { id: decoded.id } }).then((user) => {
-        req.user = user;
-        next();
-      });
-    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+      console.log("decodeToken-->", decodeToken);
+      if (!err && decodeToken) {
+        User.findOne({
+          where: {
+            id: decodeToken.id,
+          },
+        })
+          .then((user) => {
+            console.log("user-->", user);
+            if (!user) throw err;
+            console.log("req-->", req);
+            req.user = user;
+            return next();
+          })
+          .catch((err) => next(err));
+      } else {
+        req.errors = err;
+        return res.status(500).send("Not Authorized");
+      }
+    });
   }
 };
+module.exports = validateSession
 
 module.exports = validateSession;
